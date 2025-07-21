b28be2ed099c5aa2f2f5bf38b096415457d33683
Denyson <132364948+DenysonPlacido@users.noreply.github.com>
2025-07-21 16:03:03 +0000

    modified:   requirements.txt

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
+from vercel_flask import VercelHandler  # <- importante
 
-app = Flask(__name__, static_folder='../static', template_folder='../templates')
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
 def index():
@@ -11,6 +12,7 @@ def concatenar():
     data = request.json
     dados_brutos = data['valores']
     tipo = data['tipo']
+    nome_coluna = data.get('coluna', 'coluna')
 
     valores = [v.strip() for v in dados_brutos.splitlines() if v.strip()]
     blocos = []
@@ -19,10 +21,10 @@ def concatenar():
     for i in range(0, len(valores), 1000):
         grupo = valores[i:i + 1000]
         grupo_formatado = ','.join(f"{aspas}{v}{aspas}" for v in grupo)
-        blocos.append(f"coluna IN ({grupo_formatado})")
+        blocos.append(f"{nome_coluna} IN ({grupo_formatado})")
 
-    resultado = " OR ".join(blocos)
+    resultado = "\n OR ".join(blocos)
     return jsonify({"resultado": resultado})
 
-# Vercel usa 'app' como export
-handler = app
+
+handler = VercelHandler(app)
