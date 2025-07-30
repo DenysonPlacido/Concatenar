c23bbe58cda97e81b8fced00a99ede6948cf001a
Denyson <132364948+DenysonPlacido@users.noreply.github.com>
2025-07-30 13:22:27 -0400

    19

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, send_from_directory
 
-app = Flask(__name__, static_url_path="/static", static_folder="static", template_folder="templates")
+app = Flask(__name__, static_url_path="api/static", static_folder="static", template_folder="templates")
 
 @app.route('api/static/<path:filename>')
 def static_files(filename):
