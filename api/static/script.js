4ddd0b53d7d02639789332644bd7f02706e6c61d
Denyson <132364948+DenysonPlacido@users.noreply.github.com>
2025-08-19 01:30:39 +0000

    modified:   api/static/script.js 	modified:   api/static/style.css

--- api/static/script.js
+++ api/static/script.js
@@ -5,13 +5,25 @@ function mostrarAba(id) {
     event.currentTarget.classList.add('active');
 }
 
-function copiarResultado(id) {
+function copiarResultado(id, botao) {
     const el = document.getElementById(id);
     let texto = '';
+
     if (el.tagName === 'DIV' || el.tagName === 'PRE') {
         texto = el.innerText;
     } else {
         texto = el.value;
     }
-    navigator.clipboard.writeText(texto);
+
+    navigator.clipboard.writeText(texto).then(() => {
+        // muda o botão para verde
+        botao.classList.add("copied");
+        botao.innerText = "Copiado!";
+
+        // volta ao normal depois de 2s
+        setTimeout(() => {
+            botao.classList.remove("copied");
+            botao.innerText = "Copiar";
+        }, 2000);
+    });
 }
